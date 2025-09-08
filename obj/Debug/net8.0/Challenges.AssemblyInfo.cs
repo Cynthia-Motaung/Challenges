200ae8663436cac9ab82f8376e1456c49baf4a16
Cynthia-Motaung <cynthiamotaung015@gmail.com>
2025-09-08 11:31:41 +0200

    latest updates

--- obj/Debug/net8.0/Challenges.AssemblyInfo.cs
+++ obj/Debug/net8.0/Challenges.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Challenges")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83822239328b127805571317057e08410460917e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56b8ce114fd2f5101146182d726660cc7bfe9393")]
 [assembly: System.Reflection.AssemblyProductAttribute("Challenges")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Challenges")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
